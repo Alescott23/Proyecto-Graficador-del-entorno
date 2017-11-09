41df5b723a4a7f9cee8ad8e63871acc6f813b66a
Trago18 <32122377+Trago18@users.noreply.github.com>
2017-11-09 15:35:46 -0600

    Add files via upload

---
+++ Sensor Laser/Servo_SensorLaser.ino
@@ -0,0 +1,39 @@
+
+#include <Servo.h>
+#include <Wire.h>
+#include <VL53L0X.h>
+
+VL53L0X sensor;
+Servo servo;
+
+int a = 0;
+int dir = 5;
+
+void setup()
+{
+  Serial.begin(9600);
+  servo.attach(9);
+  servo.write(0);
+  
+  Wire.begin();
+  sensor.init();
+  sensor.setTimeout(500);
+  sensor.startContinuous(10);
+}
+
+void loop()
+{
+  long distancia = sensor.readRangeContinuousMillimeters();
+  
+  Serial.print(distancia);
+  Serial.print(",");
+  Serial.println(a);
+
+  a+=dir;
+  servo.write(a);
+  
+  if(a==0)dir=5;
+  if(a==180)dir=-5;
+
+  delay(10);
+}
